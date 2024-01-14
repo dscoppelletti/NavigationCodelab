5cc344fb10adc568db473607f5551771984e5169
Dario Scoppelletti <dario@scoppelletti.it>
2024-01-14 22:08:15 +0100

    4. Migrating to Compose Navigation

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,7 +19,6 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -28,6 +27,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -47,7 +49,23 @@ class RallyActivity : ComponentActivity() {
 @Composable
 fun RallyApp() {
     RallyTheme {
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
+        var currentScreen: RallyDestination by
+            remember { mutableStateOf(Overview) }
+        /* BEGIN-4.2 - Set up the NavController */
+        // The NavController is the central component when using Navigation in
+        // Compose. It keeps track of back stack composable entries, moves the
+        // stack forward, enables back stack manipulation, and navigates between
+        // destination states. Because NavController is central to navigation,
+        // it has to be created as a first step in setting up Compose
+        // Navigation.
+        // You should always create and place the NavController at the top level
+        // in your composable hierarchy, usually within your App composable.
+        // Then, all composables that need to reference the NavController have
+        // access to it. This follows the principles of state hoisting and
+        // ensures the NavController is the main source of truth for navigating
+        // between composable screens and maintaining the back stack.
+        val navController = rememberNavController()
+        /* END-4.2 */
         Scaffold(
             topBar = {
                 RallyTabRow(
@@ -57,9 +75,50 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
+            /* BEGIN-4.4 - Calling the NavHost composable with the navigation
+            graph */
+            // The 3 main parts of Navigation are the NavController, NavGraph,
+            // and NavHost. The NavController is always associated with a single
+            // NavHost composable. The NavHost acts as a container and is
+            // responsible for displaying the current destination of the graph.
+            // As you navigate between composables, the content of the NavHost
+            // is automatically recomposed. It also links the NavController with
+            // a navigation graph (NavGraph) that maps out the composable
+            // destinations to navigate between. It is essentially a collection
+            // of fetchable destinations.
+//            Box(Modifier.padding(innerPadding)) {
+//                currentScreen.screen()
+//            }
+            NavHost(
+                navController = navController,
+                // Route to know which destination to show when the app is
+                // launched
+                startDestination = Overview.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                // builder for defining and building the navigation graph
+                /* BEGIN-4.3 - Routes in Compose Navigation */
+                // When using Navigation within Compose, each composable
+                // destination in your navigation graph is associated with a
+                // route. Routes are represented as Strings that define the path
+                // to your composable and guide your navController to land on
+                // the right place. You can think of it as an implicit deep link
+                // that leads to a specific destination. Each destination must
+                // have a unique route.
+                /* END-4.3 */
+                /* BEGIN-4.5 - Adding destinations to the NavGraph */
+                composable(route = Overview.route) {
+                    Overview.screen()
+                }
+                composable(route = Accounts.route) {
+                    Accounts.screen()
+                }
+                composable(route = Bills.route) {
+                    Bills.screen()
+                }
+                /* END-4.5 */
             }
+            /* END-4.4 */
         }
     }
 }
